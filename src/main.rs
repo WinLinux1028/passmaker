3c274943901d19dde68ab96536a9a1a7ff01126a
WinLinux1028 <reimu@hakurei.win>
2023-07-11 14:17:07 +0900

    aaa

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
-#![feature(non_ascii_idents)]
-
 use rand::Rng;
 use std::io::Write;
 
-const 文字: &str = r##"0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "##;
+const CHARACTERS: &str = r##"0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "##;
 
-fn main() -> !{
-    println!("パスワード生成ウィザード passmaker 0.1.1");
+fn main() -> ! {
+    println!("パスワード生成ウィザード passmaker 0.1.2");
     loop {
         print!("どの乱数を生成する?\n1.全ASCII文字 2.空白以外 3.記号以外 4.数字のみ\n> ");
-        let _ = std::io::stdout().flush();
-        let mut 入力 = String::new();
-        let _ = std::io::stdin().read_line(&mut 入力);
-        let 範囲;
-        match 入力.trim() {
-            "1" => 範囲 = 0..95,
-            "2" => 範囲 = 0..94,
-            "3" => 範囲 = 0..62,
-            "4" => 範囲 = 0..10,
+        std::io::stdout().flush().unwrap();
+        let mut mode = String::new();
+        std::io::stdin().read_line(&mut mode).unwrap();
+        let range = match mode.trim() {
+            "1" => 0..95,
+            "2" => 0..94,
+            "3" => 0..62,
+            "4" => 0..10,
             _ => {
                 eprintln!("エラー!");
                 continue;
             }
-        }
+        };
+
         print!("何文字生成する?\n> ");
-        let _ = std::io::stdout().flush();
-        let mut 入力 = String::new();
-        let _ = std::io::stdin().read_line(&mut 入力);
-        let 入力: usize = match 入力.trim().parse(){
+        std::io::stdout().flush().unwrap();
+        let mut length = String::new();
+        std::io::stdin().read_line(&mut length).unwrap();
+        let length: usize = match length.trim().parse() {
             Ok(o) => o,
             Err(e) => {
                 eprintln!("{}", e);
                 continue;
             }
         };
-        let mut カウンタ: usize = 0;
+
         print!("パスワードは“");
-        while 入力 > カウンタ {
-            let 選択: usize = rand::rngs::OsRng.gen_range(範囲.clone());
-            print!("{}", &文字[選択..選択 + 1]);
-            カウンタ += 1;
+        for _ in 0..length {
+            let index: usize = rand::rngs::OsRng.gen_range(range.clone());
+            print!("{}", &CHARACTERS[index..index + 1]);
         }
         print!("”です\n");
     }
